d1b838c6c3c60295abae9d55a58b6bee4f468902
Muhammad-Salmun <muhammedsalmun@gmail.com>
2021-06-17 20:52:32 +0530

    updated code
    changed int to byte and bool to save more dynamic memory

--- Pulseoximeter.ino
+++ Pulseoximeter.ino
@@ -14,20 +14,19 @@ Adafruit_SSD1306 display(128, 32, &Wire, -1);
 
 
 #if ENABLE_MAX30100
-#define REPORTING_PERIOD_MS     5000
 PulseOximeter pox;
 #endif
 
 
-uint32_t tsLastReport = 0;
-int placed;
+bool tsLastReport = 0;
+bool placed;
 
 
 // Callback (registered below) fired when a pulse is detected
 void onBeatDetected()
 {
   Serial.println("Beat!");
-  if (placed == 1) 
+  if (placed == 0) 
   {
     display.clearDisplay();
     display.setFont(&FreeSerif9pt7b);
@@ -36,14 +35,14 @@ void onBeatDetected()
     display.setCursor(7, 16);
     display.print("Calculating...");    /////////////////////////////////////////////////////printing Calculating
     display.display();
-    placed++;
+    placed = 1;
   }
 }
 
 void setup()  ///////////////////////////////////////////////////////////////////////////void setup
 {
   Serial.begin(115200);
-  Serial.println("SSD1306 128x64 OLED TEST");
+  Serial.println("SSD1306 128x32 OLED TEST");
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -84,26 +83,50 @@ void loop()       //////////////////////////////////////////////////////////////
 #if ENABLE_MAX30100
   // Make sure to call update as fast as possible
   pox.update();
-  int bpm = 0;
-  int spo2 = 0;
+  byte bpm = 0;
+  byte spo2 = 0;
   // Asynchronously dump heart rate and oxidation levels to the serial
   // For both, a value of 0 means "invalid"
-  if (millis() - tsLastReport > REPORTING_PERIOD_MS)         //printing can be delayed but not pox.update()
+  if (millis() - tsLastReport > 5)         //printing can be delayed but not pox.update()
  {
     bpm = pox.getHeartRate();
     spo2 = pox.getSpO2();
     Serial.println(bpm);    
     Serial.println(spo2);
   
-    if (bpm>30 and bpm<220 and spo2>30 and spo2<100)  display_data(bpm, spo2);
+    if (bpm>30 and bpm<220 and spo2>30 and spo2<100)  
+    {
+        display.clearDisplay();
+        display.setFont();
+        display.setTextSize(1);      // Normal 1:1 pixel scale
+        
+        display.setCursor(13, 3); 
+        display.print("%SpO");          //////////////////////////////////////////////SpO2%
+        display.setFont(&Org_01);
+        display.print("2"); 
+    
+        display.setFont(&FreeSerif12pt7b);
+        display.setCursor(20, 30);
+        display.print(spo2);          ////////////////////////////////////////////////SpO2_value
+    
+        
+        display.setFont();
+        display.setCursor(70, 3); 
+        display.print("PRbpm");         //////////////////////////////////////////////PRbpm
+             
+      
+        display.setFont(&FreeSerif12pt7b);
+        display.setCursor(77, 30);
+        display.print(bpm);          ////////////////////////////////////////////////BPM_value
+        
+        display.display();         //print in display 
+    }
     
     if (bpm == 0 and spo2 == 0) 
     {
-        placed = 1;
+        placed = 0;
         display.clearDisplay();
         display.setFont(&FreeSerif9pt7b);
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
         display.setCursor(25, 13);       /////////////////////////////////////////////////////asking to place finger
         display.print("Place Your");
         display.setCursor(40,26);
@@ -112,41 +135,8 @@ void loop()       //////////////////////////////////////////////////////////////
     }
     
     tsLastReport = millis();
+    Serial.println(tsLastReport);
     
   }
 #endif
-}
-
-void display_data(int bpm, int SpO2)   /////////////////////////////////////////////////////////////////void display
-{
-    display.clearDisplay();
-    display.setFont();
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(WHITE); // Draw white text
-    display.setCursor(13, 3); 
-    display.print("%SpO");          //////////////////////////////////////////////SpO2%
-    display.setFont(&Org_01);
-    display.print("2"); 
-
-    display.setFont(&FreeSerif12pt7b);
-    display.setCursor(20, 30); 
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(WHITE); // Draw white text
-    display.print(SpO2);          ////////////////////////////////////////////////SpO2_value
-
-    
-    display.setFont();
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(WHITE); // Draw white text
-    display.setCursor(70, 3); 
-    display.print("PRbpm");         //////////////////////////////////////////////PRbpm
-         
-  
-    display.setFont(&FreeSerif12pt7b);
-    display.setCursor(77, 30); 
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(WHITE); // Draw white text
-    display.print(bpm);          ////////////////////////////////////////////////BPM_value
-    
-    display.display();         //print in display    
 }
